c3db18721c94c4d4cbd69cab5fcf56a27d5cc409
z4ire <landyzaire@email.com>
2025-01-08 11:27:45 -0300

    index

--- routes/home.py
+++ routes/home.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, render_template
 
 bp_home_route = Blueprint("home", __name__)
 
-@bp_home_route.route('/index', methods=['GET', 'POST'])
+@bp_home_route.route('/', methods=['GET', 'POST'])
 def home():
     
     return render_template("index.html")
